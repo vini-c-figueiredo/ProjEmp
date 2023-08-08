56fe9523932b2268f32176af1acffef52350b686
Vinicius Figueiredo <viniciusfigueiredo.rp2@gmail.com>
2023-08-08 19:20:52 -0300

    Validação de token

---
+++ src/app/middleware/VerifyJWT.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+class AuthJWT {
+    async verify(request, response, next) {
+        const Secret = process.env.Secret;
+        const token = request.headers['x-access-token'];
+        jwt.verify(token, Secret, (err, decoded ) => {
+            if(err) {
+                return response.status(401).end();
+            }
+
+            request.userId = decoded.userId;
+            next();
+        });
+
+
+
+
+
+        
+
+
+    }
+
+
+}
+
+
+module.exports = new AuthJWT();
--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,21 @@ const { Router } = require('express');
 const EmpController = require('./app/controller/EmpController');
 const UserController = require('./app/controller/UserController');
 const AuthController = require('./app/controller/AuthController');
+const JWT = require('./app/middleware/VerifyJWT');
 
 
 const router = Router();
 
-router.post('/new', EmpController.create);
-router.get('/tot', EmpController.index);
-router.delete('/del', EmpController.delete);
+//Auth
+router.get('/Auth', AuthController.Token);
+
+//Rotas Processo
+router.post('/new', JWT.verify, EmpController.create);
+router.get('/tot', JWT.verify, EmpController.index);
+router.delete('/del', JWT.verify, EmpController.delete);
 
 //User Login
 router.post('/newUser', UserController.create);
 router.delete('/DelUser', UserController.delete);
-
-//Auth
-router.get('/Auth', AuthController.Token);
 
 module.exports = router;
