3c7ada6f2bd4f52dc10b8481a75b84f59e3bf434
Vinicius Figueiredo <viniciusfigueiredo.rp2@gmail.com>
2023-08-17 21:48:26 -0300

    testes

--- src/app/middleware/VerifyJWT.js
+++ src/app/middleware/VerifyJWT.js
@@ -4,12 +4,14 @@ require('dotenv').config();
 class AuthJWT {
     async verify(request, response, next) {
         const Secret = process.env.Secret;
-        console.log('teste');
+        console.log('Entrou verify');
         const { token } = request.body;
         jwt.verify(token, Secret, (err, decoded ) => {
             if(err) {
+                console.log('Deu ruim');
                 return response.status(401).end();
-            }
+            };
+            console.log('Deu bom');
 
             request.userId = decoded.userId;
             next();
